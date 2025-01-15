1c425e89a1616174c065c9e8265908aa086a4de2
Tushar <tusharjoshi51997@gmail.com>
2025-01-14 22:27:51 -0500

    before Edit option

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+
+        <h1>About</h1>
+        <p>Welcome to the React Js blog's about page</p>
+    </main>
+  )
+}
+
+export default About
